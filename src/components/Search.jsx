306584e0f437b2e9db7cd7dd3a0fd4ef6fdc1d7c
Moises <MoisesTabar.013@gmail.com>
2021-03-24 13:07:55 -0400

    Styling components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,14 @@
+import '../styles/Search.css';
 import React, { useState } from 'react';
 import axiosClient from '../config/axios.config';
 
 function Search(){
     return(
-        <h1>Hello World</h1>
+        <div className="search-wrapper">
+            <div className="control has-icons-left has-icons-right">
+                <input className="input is-normal" type="text" placeholder="Example: NodeJS, Python" />
+            </div>
+        </div>
     )
 }
 
